ac54171004dd63ed8ec4605064807763fb6bd82d
Xing Ling <hylxjackson@gmail.com>
2021-09-27 19:29:12 -0400

    add homework class

---
+++ src/main/java/Homework.java
@@ -0,0 +1,9 @@
+public class Homework implements Tradable, Domesticatable{
+    public int getPrice() {
+        return 5000;
+    }
+
+    public String sound() {
+        return "Moooow";
+    }
+}
