5efee969d1606c007684ed66d319731810f32e6e
Xing Ling <hylxjackson@gmail.com>
2021-09-27 19:56:32 -0400

    add @Override keyword as ziqi pointed out! good point!

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,8 +1,10 @@
 public class Homework implements Tradable, Domesticatable{
+    @Override
     public int getPrice() {
         return 5000;
     }
 
+    @Override
     public String sound() {
         return "Moooow";
     }
