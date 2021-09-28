61c0e17e7fbdeada452c7e0e48dda630de318f15
Chloe0001 <chloe88huang@gmail.com>
2021-09-28 08:03:26 +0800

    Add tests documentation.

--- src/test/java/ChickenTest.java
+++ src/test/java/ChickenTest.java
@@ -10,11 +10,17 @@ public class ChickenTest {
         c = new Chicken();
     }
 
+    /**
+     * Test if the sound of Chicken matches "Woo-Woo."
+     */
     @Test(timeout = 50)
     public void TestSound() {
         assertEquals("Woo-Woo!", c.sound());
     }
 
+    /**
+     * Test if the price of Chicken is 5.
+     */
     @Test(timeout = 50)
     public void testGetPrice() {
         assertEquals(5, c.getPrice());
