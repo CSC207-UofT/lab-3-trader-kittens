6faf7560298785125df6f3553830e2d28b66d67b
Xing Ling <hylxjackson@gmail.com>
2021-09-27 20:05:49 -0400

    add comments in HomeworkTest.java

--- src/test/java/HomeworkTest.java
+++ src/test/java/HomeworkTest.java
@@ -11,11 +11,13 @@ public class HomeworkTest {
         m = new Homework();
     }
 
+//    Test whether Homework makes the right sound
     @Test(timeout = 50)
     public void TestSound() {
         assertEquals("Moooow!", m.sound());
     }
 
+//    Test whether Homework has the right price
     @Test(timeout = 50)
     public void TestGetPrice() {
         assertEquals(5000, m.getPrice());
